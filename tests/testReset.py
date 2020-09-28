cdad212bfaa096bcd3474a09afd8a6b8ffcd36c2
Joshua Hessing <jjhjjh7@gmail.com>
2020-09-28 17:06:46 -0600

    Edited testReset to include a function to delete any file and one to delete the data file

--- tests/testReset.py
+++ tests/testReset.py
@@ -2,9 +2,11 @@ import os
 import sys
 from pathlib import Path
 
-base = Path(__file__).parent #sets base as the current file directory
-file_path = (base / "resetText.txt").resolve()#sets the file path to the base
-file = open(file_path, 'r+') #opens the file using the file path
-file.truncate(0) #This is to delete everything
-file.close() #This closes the file so it is no longer being edited
-print("We ran") #Shows that the program is finished
+def fileReset(file_path): 
+    file = open(file_path, 'r+') #opens the file using the function input
+    file.truncate(0) #This is to delete everything
+    file.close() #This closes the file so it is no longer being edited
+def dataReset(): #Deletes all data files
+    fileReset("flightLogic/data/Attitude_Data.txt") 
+    fileReset("flightLogic/data/Deploy_Data.txt")
+    fileReset("flightLogic/data/TTNC_Data.txt")
