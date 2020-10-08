bed1b6b1b29798971a51f858e104d480679d0a74
Joshua Hessing <jjhjjh7@gmail.com>
2020-10-08 12:44:40 -0600

    Began working on exceptions for fileReset code

--- tests/testReset.py
+++ tests/testReset.py
@@ -1,11 +1,22 @@
 import os
 import sys
 
-def fileReset(file_path): 
-    file = open(file_path, 'r+') #opens the file using the function input
-    file.truncate(0) #This is to delete everything
-    file.close() #This closes the file so it is no longer being edited
-def dataReset(): #Deletes all data files
-    fileReset("flightLogic/data/Attitude_Data.txt") 
+def fileReset(file_path):
+    try:
+        #opens and erases file, if there is no file, it will create the file
+        file = open(file_path, 'w') 
+    except FileNotFoundError:
+        print("Uh oh spaghettios, if this is happening we're all screwed.")
+    except SyntaxError:
+        print("There was a syntax error and we should re run the program")
+        print(" because if this is happening in space it just means a bit error")
+    except OSError:
+        print("Not gonna lie not sure what would cause this one")
+    else:
+        file.close() #This closes the file so it is no longer being edited
+def fullReset(): #Deletes all data files
+    fileReset("flightLogic/data/Attitude_Data.txt")
     fileReset("flightLogic/data/Deploy_Data.txt")
     fileReset("flightLogic/data/TTNC_Data.txt")
+
+fileReset("tests/resetText.txt")
