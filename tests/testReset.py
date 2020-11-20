50973457bc9754607b962056ab4bc4c5f76eefad
Joshua Hessing <jjhjjh7@gmail.com>
2020-11-19 20:57:52 -0700

    Changed testReset.py to check if each file exists and to only reset them if they don't exist. Updated the README to match

--- tests/testReset.py
+++ tests/testReset.py
@@ -3,26 +3,27 @@ import sys
 import asyncio
 
 class FileReset():
-    """Run FileReset.fullReset() in asyncio to automatically reset all files."""
+    """Checks to ensure that data files exist after reboot. Use "asyncio.run(FileReset.fullReset())" to run.
+    
+    Includes program to manually reset individual files for testing purposes. Use "asyncio.run(FileReset.fullReset())" to run."""
 
-    #List of file paths as specified by Jack
+    #List of file paths
     FILE_PATHS = [ 
         "TXISR/data/flagsFile.txt" , 
         "TXISR/data/txFile.txt" , 
         "TXISR/data/txWindows.txt" , 
+        "TXISR/data/transmissionFlag.txt" , 
+        "TXISR/data/AX25Flag.txt",
         "flightLogic/backupBootRecords" , 
         "flightLogic/bootRecords" , 
         "flightLogic/data/Attitude_Data.txt" , 
         "flightLogic/data/Deploy_Data.txt" , 
-        "flightLogic/data/TTNC_Data.txt" , 
-        "TXISR/data/transmissionFlag.txt" , 
-        "TXISR/data/AX25Flag.txt"]
+        "flightLogic/data/TTNC_Data.txt" ,]
 
-    async def reset(self, file_path):
+    def reset(self, file_path):
         """Opens and erases file, certain files are then filled with required text. If there is no file under a certain file path, it will create the file."""
-        file = open(file_path, 'w')
-        await asyncio.sleep(1)
         #Depending on the file path, replaces the empty file with a string of text based on what data will be written in the file
+        file = open(file_path, 'w')
         if file_path == "flightLogic/backupBootRecords" or file_path == "flightLogic/bootRecords":
             file.write("0\n0\n0\n")
         if file_path == "TXISR/data/flagsFile.txt":
@@ -31,51 +32,32 @@ class FileReset():
             file.write("Enabled")
         if file_path == "TXISR/data/AX25Flag.txt":
             file.write("Disabled")
-        await asyncio.sleep(1)
-        file.close() 
-        #This closes the file so it is no longer being edited
-        await asyncio.sleep(1)
-
-    async def fill(self, i):
-        """For testing purposes, fills given files with random stuff so we can erase it."""
-        file = open(i, 'a')
-        await asyncio.sleep(1)
-        file.write("Picanha hamburger flank, biltong shankle tri-tip brisket fatback sausage pig sirloin tail ham venison. Cow bacon kielbasa capicola beef tail ham prosciutto. Doner andouille beef landjaeger, buffalo boudin tail strip steak sirloin cow bacon. Kevin chicken venison ribeye spare ribs strip steak, pig t-bone pork chop meatball bresaola meatloaf landjaeger rump swine. Short ribs pastrami ham meatball.")
-        await asyncio.sleep(1)
         file.close()
-        await asyncio.sleep(1)
+        #This closes the file so it is no longer being edited
     
     @classmethod
     async def fullReset(self):
-        """Runs reset for all file paths that Jack gave us."""
-        await asyncio.gather(
-            self.reset(self, self.FILE_PATHS[0]),
-            self.reset(self, self.FILE_PATHS[1]),
-            self.reset(self, self.FILE_PATHS[2]),
-            self.reset(self, self.FILE_PATHS[3]),
-            self.reset(self, self.FILE_PATHS[4]),
-            self.reset(self, self.FILE_PATHS[5]),
-            self.reset(self, self.FILE_PATHS[6]),
-            self.reset(self, self.FILE_PATHS[7]),
-            self.reset(self, self.FILE_PATHS[8]),
-            self.reset(self, self.FILE_PATHS[9])
-        )
-        print("Done")
+        """Checks to make sure that files necessary for reboot exist, if they don't, it creates them."""
+        #Runs through every file in FILE_PATHS list.
+        for i in self.FILE_PATHS:
+            #Opens the file to see if it exists.
+            try:
+                file = open(i)
+            #If it doesn't, it runs reset to create it.
+            except OSError:
+                self.reset(self, i)
+                print("Created " + i)
+                await asyncio.sleep(0)
+            #Otherwise it just closes the file.
+            else:
+                file.close()
+                await asyncio.sleep(0)
 
     @classmethod
-    async def fullFill(self):
-        await asyncio.gather(
-            self.fill(self, self.FILE_PATHS[0]),
-            self.fill(self, self.FILE_PATHS[1]),
-            self.fill(self, self.FILE_PATHS[2]),
-            self.fill(self, self.FILE_PATHS[3]),
-            self.fill(self, self.FILE_PATHS[4]),
-            self.fill(self, self.FILE_PATHS[5]),
-            self.fill(self, self.FILE_PATHS[6]),
-            self.fill(self, self.FILE_PATHS[7]),
-            self.fill(self, self.FILE_PATHS[8]),
-            self.fill(self, self.FILE_PATHS[9])
-        )
-        print("Done")
+    async def individualReset(self, file_path):
+        """Allows the manual reset of a single file."""
+        #Runs reset once for a single file.
+        self.reset(self, file_path)
+        await asyncio.sleep(0)
 
 asyncio.run(FileReset.fullReset())
--- tests/testResetREADME.md
+++ tests/testResetREADME.md
@@ -2,32 +2,36 @@ GASPACS Software : testReset.py
 ==
 Opening Notes:
 --
-When the satilite is in orbit, there will not be enough data storage on the satilite to continually store more and more data without a generic data reset class. When the data is taking up a certain portion of the total avaliable storage on the satilite, and after all data has been transferred back to ground, there needs to be a way to reset all of these files while the satilite is in orbit. 
+When something goes wrong enough for the pi to reset a check needs to be done to ensure that the data files exist and create them if it does not. This program will do just that, running through a set list of programs, checking to ensure the exist, and creating them if they do not. 
 
 Testing:
 --
 Location: 
 --
+/tests/testReset.py
+
+Full Reset:
+
 Functionality:
 --
-The files that will need to be reset periodically while the satilite is in space are:
+    The files that will need to be reset periodically while the satilite is in space are:
 
-*TXISR/data/flagsFile.txt
- TXISR/data/txFile.txt
- TXISR/data/txWindows.txt
-*flightLogic/backupBootRecords
-*flightLogic/bootRecords
- flightLogic/data/Attitude_Data.txt
- flightLogic/data/Deploy_Data.txt
- flightLogic/data/TTNC_Data.txt
-*TXISR/data/transmissionFlag.txt 
-*TXISR/data/AX25Flag.txt
+    *TXISR/data/flagsFile.txt
+    TXISR/data/txFile.txt
+    TXISR/data/txWindows.txt
+    *TXISR/data/transmissionFlag.txt 
+    *TXISR/data/AX25Flag.txt
+    *flightLogic/backupBootRecords
+    *flightLogic/bootRecords
+    flightLogic/data/Attitude_Data.txt
+    flightLogic/data/Deploy_Data.txt
+    flightLogic/data/TTNC_Data.txt
 
-The stared files are those which need to have certain strings added in after the files have been completely reset, resetFile.py does this. This class also runs asynchronously, meaning that this class can be called even if another asynchronous program is running, this one can run too.
+    * files have strings added to them after being reset for proper functionality.
+
+    The program runs through the list of files trying to open each one. If the file does not exist then an OS error will be thrown which the except clause will catch and reopen the file using the 'w' argument which creates the file when it's not found.
 
 Questions:
 --
-Why does this need to run asynchronously?
-Where does this program run asynchronously?
-Do we need to have a safety system in place to prevent this class from being run accidentaly before all of the data is transfered back to ground?
-
+Where does this program run?
+Do we need to incorporate this program into some other program?
