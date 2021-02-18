391477ac8269deff72f815c967f5e8eda60ea69b
Joshua Hessing <jjhjjh7@gmail.com>
2021-02-18 12:50:06 -0700

    Changed for testing on py

--- DummyDrivers/Accelerometer/Accelerometer.py
+++ DummyDrivers/Accelerometer/Accelerometer.py
@@ -8,20 +8,27 @@
 #     sudo pip3 install adafruit-circuitpython-lsm303dlh-mag
 
 
-#from DummyDrivers.Driver import Driver
+#from Drivers.Driver import Driver
 import random
-#import board
-#import busio
-#import adafruit_lsm303_accel
+"""import board
+import busio
+import adafruit_lsm303_accel"""
+import sys
 
-class Accelerometer(Driver):
+#class Accelerometer(Driver):
+class Accelerometer:
   #Set up I2C link
   #i2c = busio.I2C(board.SCL, board.SDA)
-
+  """
   def __init__(self):
     super().__init__("Accelerometer")
+  """
 
   def read(self):
-    return (round(random.randint(0, 10) + random.random(), 2), 
-            round(random.randint(0, 10) + random.random(), 2), 
-            round(random.randint(0, 10) + random.random(), 2))
+    #accel = adafruit_lsm303_accel.LSM303_Accel(self.i2c)
+    i = float(round(random.randint(0, 10) + random.random(), 2))
+    print (i)
+    print (sys.getsizeof(i))
+
+a = Accelerometer()
+a.read()
