02fb6970aad69158a30c5b2c21a7e0b89ac5be88
Joshua Hessing <jjhjjh7@gmail.com>
2021-02-19 14:50:13 -0700

    Modified Accelerometer dummy driver to have option of returning three situations. Normal output, accelerometer not returning data, and accelerometer returning wrong size of data.

--- DummyDrivers/Accelerometer/Accelerometer.py
+++ DummyDrivers/Accelerometer/Accelerometer.py
@@ -1,3 +1,4 @@
+"""
 #Make sure the following libraries are installed:
 #   sudo pip3 install RPI.GPIO
 #   sudo pip3 install adafruit-blinka
@@ -7,36 +8,54 @@
 #   For LSM303DLH:
 #     sudo pip3 install adafruit-circuitpython-lsm303dlh-mag
 
+Warning: dummy driver is currently set to only run by itself.
+It will not be able to run in flight logic without modification
+
+Single line comments  are original driver code commented out for the sake of the
+dummy driver. String literals are comments explaining code function
+"""
 
 #from Drivers.Driver import Driver
-import random
-"""import board
-import busio
-import adafruit_lsm303_accel"""
+#import board
+#import busio
+#import adafruit_lsm303_accel
+
 import sys
+import random
 
 #class Accelerometer(Driver):
 class Accelerometer:
   #Set up I2C link
   #i2c = busio.I2C(board.SCL, board.SDA)
-  """
-  def __init__(self):
-    super().__init__("Accelerometer")
-  """
-
+  
+  #def __init__(self):
+    #super().__init__("Accelerometer")"""
+  
   def read(self, t):
-    #accel = adafruit_lsm303_accel.LSM303_Accel(self.i2c)
+    """
+    Function that reads and returns accelerometer data
+      Dummy driver has been modified to simulate three situations:
+      normal values (0), accelerometer crashes and returns nothing (1),
+      and incorrect size (2).
+      Run using Accelerometer.read(<sitation type>)
+    """
     try:
+      #accel = adafruit_lsm303_accel.LSM303_Accel(self.i2c)
       if (t == 0):
-        i = float(round(random.randint(0, 10) + random.random(), 2))
+        accel = float(round(random.randint(1, 10) + random.random(), 2))
+      elif (t == 1):
+        accel = i
       else:
-        i = int(random.randint(0, 10))
-    except (sys.getsizeof(i)!=16):
+        if sys.getsizeof(accel[0]) != sys.getsizeof(float(-1.0)):
+          raise byteSize
+        else:
+          accel = int(random.randint(1, 10))
+    except:
       print(float(0.0))
+      return(float(0.0))
     else:
-      print(i)
-    
+      print(accel)
+      return(accel)
 
-a = Accelerometer()
-a.read(0)
-a.read(1)
+class byteSize(Exception):
+  pass
